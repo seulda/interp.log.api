02edc2653c857abc38bcae368131b8ccc21abaf5
keyy1315 <lovelina1315@gmail.com>
2025-03-05 09:52:55 +0900

    feat: mapStruct.updateRefreshToken 메소드에서 updateMember 객체 중 값이 null 로 들어올 때 무시하도록 빈 매핑 설정

--- src/main/java/net/devgrr/interp/log/api/config/mapStruct/MemberMapper.java
+++ src/main/java/net/devgrr/interp/log/api/config/mapStruct/MemberMapper.java
@@ -41,15 +41,9 @@ public interface MemberMapper {
 
   MemberResponse toResponse(Member member);
 
-  @Mapping(target = "id", ignore = true)
-  @Mapping(target = "password", ignore = true)
-  @Mapping(target = "name", ignore = true)
-  @Mapping(target = "email", ignore = true)
-  @Mapping(target = "image", ignore = true)
-  @Mapping(target = "role", ignore = true)
-  @Mapping(target = "isActive", ignore = true)
-  @Mapping(target = "createdDate", ignore = true)
   @Mapping(target = "refreshToken", source = "refreshToken")
+  @Mapping(target = "updatedDate", source = "updatedDate")
+  @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
   Member updateMemberRefreshToken(Member updateMember, @MappingTarget Member member);
 
   @Mapping(
