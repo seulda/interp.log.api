8f663ac490cacb82fd1aece1d0408b2b51cd0a79
keyy1315 <lovelina1315@gmail.com>
2025-03-05 10:47:32 +0900

    feat: 댓글 2depth 이상 중첩 구조 조회 로직 구현
    재귀함수로 구현

--- src/main/java/net/devgrr/interp/log/api/comment/CommentService.java
+++ src/main/java/net/devgrr/interp/log/api/comment/CommentService.java
@@ -53,6 +53,26 @@ public class CommentService {
         .collect(Collectors.toList());
   }
 
+  public List<CommentResponse> getCommentsRecursively(Integer postId) {
+    List<Comment> comments = getCommentsByPostId(postId);
+
+    List<Comment> rootComments = comments.stream()
+            .filter(comment -> comment.getParentCommentId() == null)
+            .toList();
+
+    return rootComments.stream()
+            .map(rootComment -> buildCommentHierarchy(rootComment, comments))
+            .toList();
+  }
+
+  private CommentResponse buildCommentHierarchy(Comment rootComment, List<Comment> comments) {
+      List<CommentResponse> childComments = comments.stream()
+              .filter(comment -> Objects.equals(comment.getParentCommentId(), rootComment.getId()))
+              .map(child -> buildCommentHierarchy(child, comments))
+              .toList();
+      return commentMapper.toResponseWithChildren(rootComment, childComments);
+  }
+
   public Comment existCommentById(Integer id) throws BaseException {
     return commentRepository
         .findById(id)
